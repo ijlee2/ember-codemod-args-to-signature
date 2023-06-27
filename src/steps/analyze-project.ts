f0899f92b135c0d6fa5b633400fdd0f809a7f4fa
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-27 07:56:32 +0200

    chore: Scaffolded context.signature

--- src/steps/analyze-project.ts
+++ src/steps/analyze-project.ts
@@ -1,14 +1,24 @@
 import type { Context, Options } from '../types/index.js';
 import {
   filterComponentEntities,
+  findArguments,
+  findBlocks,
   findComponentEntities,
+  findElement,
 } from './analyze-project/index.js';
 
 export function analyzeProject(options: Options): Context {
   let entities = findComponentEntities(options);
   entities = filterComponentEntities(entities, options);
 
+  const signature = {
+    Args: findArguments(options),
+    Blocks: findBlocks(options),
+    Element: findElement(options),
+  };
+
   return {
     entities,
+    signature,
   };
 }
---
+++ src/steps/analyze-project/find-blocks.ts
@@ -0,0 +1,8 @@
+import type { Options } from '../../types/index.js';
+
+// @ts-expect-error: Method to be implemented
+export function findBlocks(options: Options): Set<string> | undefined {
+  // ...
+
+  return undefined;
+}
