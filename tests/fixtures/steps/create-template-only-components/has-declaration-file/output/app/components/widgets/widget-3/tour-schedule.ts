12daeac4af44e8cc2e156fdc85d59deccb0f351a
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-27 23:32:55 +0200

    chore: Updated test fixtures (create-template-only-components)

--- tests/fixtures/steps/create-template-only-components/has-declaration-file/output/app/components/widgets/widget-3/tour-schedule.ts
+++ tests/fixtures/steps/create-template-only-components/has-declaration-file/output/app/components/widgets/widget-3/tour-schedule.ts
@@ -1,17 +1,8 @@
 import templateOnlyComponent from '@ember/component/template-only';
 
-interface WidgetsWidget3TourScheduleSignature {
-  Args: {};
-}
+interface WidgetsWidget3TourScheduleSignature {}
 
 const WidgetsWidget3TourScheduleComponent =
   templateOnlyComponent<WidgetsWidget3TourScheduleSignature>();
 
 export default WidgetsWidget3TourScheduleComponent;
-
-declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry {
-    'Widgets::Widget3::TourSchedule': typeof WidgetsWidget3TourScheduleComponent;
-    'widgets/widget-3/tour-schedule': typeof WidgetsWidget3TourScheduleComponent;
-  }
-}
