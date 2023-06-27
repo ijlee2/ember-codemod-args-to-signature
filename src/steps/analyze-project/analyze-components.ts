dca9e7698d41a1444580f39438d3df77d125ddc8
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-27 13:59:20 +0200

    chore: Scaffolded context.signatureMap

--- src/steps/analyze-project/analyze-components.ts
+++ src/steps/analyze-project/analyze-components.ts
@@ -1,14 +1,25 @@
 import type { ExtensionMap, Options, SignatureMap } from '../../types/index.js';
+import { findArguments } from './find-arguments.js';
+import { findBlocks } from './find-blocks.js';
+import { findElement } from './find-element.js';
 
 export function analyzeComponents(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // @ts-expect-error
   extensionMap: ExtensionMap,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // @ts-expect-error
   options: Options,
 ): SignatureMap {
   const signatureMap: SignatureMap = new Map();
+
+  for (const [entityName, _extensions] of extensionMap) {
+    const Args = findArguments(options);
+    const Blocks = findBlocks(options);
+    const Element = findElement(options);
+
+    signatureMap.set(entityName, {
+      Args,
+      Blocks,
+      Element,
+    });
+  }
 
   return signatureMap;
 }
