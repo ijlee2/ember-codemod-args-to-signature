6d4f9218e4e1bc9650765f4754ce4f8cb925000f
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-27 23:36:20 +0200

    chore: Updated test fixtures (create-signatures)

--- tests/fixtures/steps/create-signatures/has-declaration-file/input/app/components/tracks.ts
+++ tests/fixtures/steps/create-signatures/has-declaration-file/input/app/components/tracks.ts
@@ -1,17 +1,8 @@
 import templateOnlyComponent from '@ember/component/template-only';
 
-interface TracksSignature {
-  Args: {};
-}
+interface TracksSignature {}
 
 const TracksComponent =
   templateOnlyComponent<TracksSignature>();
 
 export default TracksComponent;
-
-declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry {
-    'Tracks': typeof TracksComponent;
-    'tracks': typeof TracksComponent;
-  }
-}
--- tests/fixtures/steps/create-signatures/has-hbs-file-only/input/app/components/ui/form/information.ts
+++ tests/fixtures/steps/create-signatures/has-hbs-file-only/input/app/components/ui/form/information.ts
@@ -1,17 +1,8 @@
 import templateOnlyComponent from '@ember/component/template-only';
 
-interface UiFormInformationSignature {
-  Args: {};
-}
+interface UiFormInformationSignature {}
 
 const UiFormInformationComponent =
   templateOnlyComponent<UiFormInformationSignature>();
 
 export default UiFormInformationComponent;
-
-declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry {
-    'Ui::Form::Information': typeof UiFormInformationComponent;
-    'ui/form/information': typeof UiFormInformationComponent;
-  }
-}
