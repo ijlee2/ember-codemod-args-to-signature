f3d442e120998820f3a2b952de74e5c48f2109dd
ijlee2 <ijlee2@users.noreply.github.com>
2023-07-12 10:49:34 +0200

    chore: Updated @typescript-eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
+    project: true,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'simple-import-sort', 'typescript-sort-keys'],
@@ -31,14 +32,15 @@ module.exports = {
     // TypeScript files
     {
       files: ['**/*.{cts,ts}'],
-      extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-      ],
+      extends: ['plugin:@typescript-eslint/recommended-type-checked'],
       rules: {
         '@typescript-eslint/array-type': 'error',
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
       },
     },
     // TypeScript and JavaScript files
@@ -55,7 +57,10 @@ module.exports = {
         browser: false,
         node: true,
       },
-      extends: ['plugin:n/recommended'],
+      extends: [
+        'plugin:@typescript-eslint/disable-type-checked',
+        'plugin:n/recommended',
+      ],
     },
   ],
 };
--- src/steps/update-signatures/update-signature.ts
+++ src/steps/update-signatures/update-signature.ts
@@ -15,6 +15,7 @@ type Data = {
 
 function getBodyNode(node: unknown, key: 'Args' | 'Blocks' | 'Element') {
   // @ts-ignore: Assume that types from external packages are correct
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return node.body.body.find((node) => {
     if (node.type !== 'TSPropertySignature' || node.key.type !== 'Identifier') {
       return false;
