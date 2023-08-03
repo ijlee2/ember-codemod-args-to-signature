987f6f3670645ba91ab200d4efdb266f0097708b
ijlee2 <ijlee2@users.noreply.github.com>
2023-08-03 15:23:09 +0200

    feature: Filled out Signature['Args']

--- src/steps/update-signatures/builders.ts
+++ src/steps/update-signatures/builders.ts
@@ -32,10 +32,21 @@ function builderConvertTsTypeToKeyword(tsType: string) {
 }
 
 export function builderCreateArgsNode(signature: Signature) {
+  const members: unknown[] = [];
+
+  (signature.Args ?? []).forEach((argumentName) => {
+    members.push(
+      AST.builders.tsPropertySignature(
+        AST.builders.identifier(argumentName),
+        AST.builders.tsTypeAnnotation(AST.builders.tsUnknownKeyword()),
+      ),
+    );
+  });
+
   return AST.builders.tsPropertySignature(
     AST.builders.identifier('Args'),
     // @ts-ignore: Assume that types from external packages are correct
-    AST.builders.tsTypeAnnotation(AST.builders.tsTypeLiteral([])),
+    AST.builders.tsTypeAnnotation(AST.builders.tsTypeLiteral(members)),
     false,
   );
 }
