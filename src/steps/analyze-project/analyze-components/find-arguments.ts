2cc86125c6000604087b52aac318c876069ab827
ijlee2 <ijlee2@users.noreply.github.com>
2023-08-03 16:16:06 +0200

    wip: Scaffolded function (analyzeClass)

--- src/steps/analyze-project/analyze-components/find-arguments.ts
+++ src/steps/analyze-project/analyze-components/find-arguments.ts
@@ -1,7 +1,24 @@
-// import { AST as ASTJavaScript } from '@codemod-utils/ast-javascript';
+import { AST as ASTJavaScript } from '@codemod-utils/ast-javascript';
 import { AST as ASTTemplate } from '@codemod-utils/ast-template';
 
 import type { Signature } from '../../../types/index.js';
+
+function analyzeClass(file?: string): Set<string> {
+  const args = new Set<string>();
+
+  if (file === undefined) {
+    return args;
+  }
+
+  // We know that the file is in TypeScript
+  const traverse = ASTJavaScript.traverse(true);
+
+  traverse(file, {
+    // ...
+  });
+
+  return args;
+}
 
 function analyzeTemplate(file: string): Set<string> {
   const traverse = ASTTemplate.traverse();
@@ -35,12 +52,8 @@ export function findArguments({
   }
 
   const argsFromTemplate = analyzeTemplate(templateFile);
-
-  if (classFile !== undefined) {
-    // const argsFromClass = analyzeClass(classFile);
-  }
-
-  const args = new Set([...argsFromTemplate]);
+  const argsFromClass = analyzeClass(classFile);
+  const args = new Set([...argsFromTemplate, ...argsFromClass]);
 
   return Array.from(args).sort();
 }
