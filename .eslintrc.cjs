3ee0dbd7cbb53cc2f767cfbdab613e63d0fdcf63
ijlee2 <ijlee2@users.noreply.github.com>
2023-12-18 17:50:05 +0100

    chore: Reconfigured eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,7 +37,8 @@ module.exports = {
       ],
       rules: {
         '@typescript-eslint/array-type': 'error',
-        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/consistent-type-imports': 'error',
+        '@typescript-eslint/no-import-type-side-effects': 'error',
         'import/no-duplicates': 'error',
       },
     },
