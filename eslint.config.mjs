ae3754e90929d2af1f75b94ca94cee7bface0bd3
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-02-10 12:03:09 +0100

    Simplified lint configurations (#56)
    * refactor: Consumed @ijlee2-frontend-configs
    
    * chore: Added lockfile
    
    * chore: Ignored type errors
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import baseConfiguration from '@ijlee2-frontend-configs/eslint-config-node/typescript';
+
+export default [
+  {
+    ignores: [
+      'dist/',
+      'dist-for-testing/',
+      'node_modules/',
+      'src/blueprints/',
+      'tests/fixtures/',
+      'tmp/',
+      '.*/',
+    ],
+  },
+  ...baseConfiguration,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+    },
+  },
+];
