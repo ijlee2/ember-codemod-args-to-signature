5dbe3ff7033637e6c77bf5077846e18a560d8ca6
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-11 12:56:04 +0200

    chore: Updated createRegistries()

--- src/steps/create-registries.ts
+++ src/steps/create-registries.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 
+import { toEcma, updateJavaScript } from '@codemod-utils/ast-template-tag';
 import { doubleColonize, pascalize } from '@codemod-utils/ember';
 import {
   createFiles,
@@ -25,23 +26,33 @@ export function createRegistries(context: Context, options: Options): void {
   for (const [componentName, extensions] of extensionMap) {
     const filePath = getClassPath(componentName, extensions, options);
 
-    const data = {
-      entity: {
-        doubleColonizedName: doubleColonize(componentName),
-        name: componentName,
-        pascalizedName: pascalize(componentName),
-      },
-    };
-
     try {
       let file = readFileSync(join(projectRoot, filePath), 'utf8');
+      const ecmaFile = toEcma(file);
 
-      if (hasRegistry(file)) {
+      if (hasRegistry(ecmaFile)) {
         continue;
       }
 
-      file = renameComponent(file, data);
-      file = createRegistry(file, data);
+      const data = {
+        entity: {
+          doubleColonizedName: doubleColonize(componentName),
+          name: componentName,
+          pascalizedName: pascalize(componentName),
+        },
+      };
+
+      if (extensions.has('.gts')) {
+        file = updateJavaScript(file, (code) => {
+          code = renameComponent(code, data);
+          code = createRegistry(code, data);
+
+          return code;
+        });
+      } else {
+        file = renameComponent(file, data);
+        file = createRegistry(file, data);
+      }
 
       fileMap.set(filePath, file);
     } catch (error) {
